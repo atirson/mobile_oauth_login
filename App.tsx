b188d5bdb9dafa1e33b89ca8ac95ce963fa872e1
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-03-18 12:58:26 -0300

    Initial Config

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as AuthSession from 'expo-auth-session';
+
+export default function App() {
+  const [user, setUser] = useState<any>()
+
+  const handleLoginSocial = async () => {
+    try {
+      const CLIENT_ID = "904952ec1414b8b81dfe";
+      const REDIRECT_URI = "https://auth.expo.io/@atirson/mobile_oauth_login";
+      const SCOPE = encodeURI("user repo");
+      const RESPONSE_TYPE = "code";
+
+      const authUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=${RESPONSE_TYPE}`;
+
+      const response = await AuthSession.startAsync({ authUrl })
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Button title='Click me' onPress={handleLoginSocial} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
